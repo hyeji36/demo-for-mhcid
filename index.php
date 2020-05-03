3bd8d41c68164d744c6c8588ee66b3e4bd185f45
hyeji36 <62810147+hyeji36@users.noreply.github.com>
2020-05-03 13:55:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php header( 'Location: /index.html' ) ;  ?>
+<img src="file:///Users/hyejijang/Downloads/1572992170-fb-img-1572904909573.jpg" style="float:center; max-width:50%; position:relative; margin:40px">
+background-image: url("file:///Users/hyejijang/Desktop/Screen%20Shot%202020-05-02%20at%209.31.49%20PM.png");
